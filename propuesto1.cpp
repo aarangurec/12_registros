c531988f8c6d57732db179d8bf261267f360047e
aarangurec <aarangurec@unjbg.edu.pe>
2026-06-24 10:21:53 -0500

    Se empezo la realizacion de los ejercicios; Libros, 1, 2, 3, 4

---
+++ propuesto1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct Empleado{
+        string Nombres;
+        string sexo;
+        float sueldo;
+int main(){
+
+
+
+    return 0;
+    }
+
+
+
+
+
