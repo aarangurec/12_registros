3c13b68461820daf23c72b20b59d297eb12a9dd9
aarangurec <aarangurec@unjbg.edu.pe>
2026-06-29 15:59:35 -0500

    Se termino el ejercicio propuesto 1, COMPLETAMENTE

--- propuesto1.cpp
+++ propuesto1.cpp
@@ -11,7 +11,11 @@ int main(){
         cout<<"Cantidad de empleados: "<<endl;
         cin>>n;
         cin.ignore();
-        for (int i; i<n; i++){
+        if (n<=0){
+                cout<<"USTED NO TIENE EMPLEADOS"<<endl;
+                return 0;
+        }
+        for (int i=0; i<n; i++){
                 cout<<"Empleado: "<<i+1<<endl;
                 cout<<"Su nombre es: ";
                 getline(cin, e[i].Nombre);
@@ -21,27 +25,35 @@ int main(){
                 cin>>e[i].sueldo;
                 cin.ignore();
         }
-        int Mayor=0, Menor=0;
+        float Mayor=e[0].sueldo, Menor=e[0].sueldo;
         for (int i=1; i<n; i++){
-                if (e[i].sueldo>e[Mayor].sueldo){
-                        Mayor=i;
+                if (e[i].sueldo>Mayor){
+                        Mayor=e[i].sueldo;
                 }      
         }
-        cout<<"EL EMPLEADO CON MAYOR SUELDO ES: "<<endl;
-        cout<<e[Mayor].sueldo<<endl;
-        cout<<e[Mayor].Nombre<<endl;
-        cout<<e[Mayor].sexo<<endl;
+        cout<<"LOS EMPLEADOS CON MAYOR SUELDO "<<Mayor<<"  SON: "<<endl;
+        for (int i=0; i<n ; i++){
+        if (e[i].sueldo==Mayor){
+        cout<<"Nombre: "<<e[i].Nombre<<endl;
+        cout<<"Sexo: "<<e[i].sexo<<endl; 
+        cout<<"Sueldo: "<<e[i].sueldo<<endl;
+        cout<<"--------------------"<<endl;
+        }
+        
+        }
+
         for (int i=1; i<n; i++){
-                if (e[i].sueldo<e[Menor].sueldo){
-                        Menor = i;
+                if (e[i].sueldo<Menor){
+                        Menor= e[i].sueldo;
                 }
         }
-        cout<<"EL EMPLEADO CON MENOR SUELDO ES: "<<endl;
-        cout<<e[Menor].sueldo<<endl;
-        cout<<e[Menor].Nombre<<endl;
-        cout<<e[Menor].sexo<<endl;
-
-
+        cout<<"LOS EMPLEADOS CON MENOR SUELDO "<<Menor<<"  SON: "<<endl;
+        for (int i=0; i<n; i++ ){
+        if(e[i].sueldo==Menor) {
+        cout<<e[i].Nombre<<endl;
+        cout<<e[i].sexo<<endl;
+        cout<<e[i].sueldo<<endl;}
+        }
 
     return 0;
     }
