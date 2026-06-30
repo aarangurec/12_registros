f19c2b6833c76cd3558092e29a884287b6f86721
aarangurec <aarangurec@unjbg.edu.pe>
2026-06-29 19:10:09 -0500

    Ejericio propuesto2 en proceso

---
+++ propuesto2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct Persona {
+    string DNI;
+    string  Nombre;
+    float edad;
+};
+int main (){
+    int n;
+    Persona p[100];
+    cout<<"El nro de Personas: "<<endl;
+    cin>>n;
+    cin.ignore();
+    for(int i=0; i<n; i++){
+        cout<<"Persona"<<"["<<i+1<<"]"<<endl;
+        cout<<"Ingrese su Nombre: "<<endl;
+        getline(cin, p[i].Nombre);
+        cout<<"Ingrese su DNI: "<<endl;
+        getline(cin, p[i].DNI);
+        cout<<"Ingrese su Edad: "<<endl;
+        cin>>p[i].edad;
+        cin.ignore();
+    }
+    system("pause");
+
+
+    return 0;
+}
